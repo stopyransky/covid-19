98675d229a1eeae090b5021eeb0875d596f22697
Karol Stopyra <stopyransky@gmail.com>
2020-03-18 12:44:47 +0100

    assets

---
+++ docs/precache-manifest.1b57ce75e2ce7fbea2c03bd7ee7edda5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8445e5eb553fbb1631ca3f5ad1f2a37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d415d250f2541e90b572",
+    "url": "/static/css/main.916507da.chunk.css"
+  },
+  {
+    "revision": "ce696a9054f7e7d9467c",
+    "url": "/static/js/2.8ea7c1ab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8ea7c1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d415d250f2541e90b572",
+    "url": "/static/js/main.a014dc37.chunk.js"
+  },
+  {
+    "revision": "beac14da8b28c9d9f000",
+    "url": "/static/js/runtime-main.0ce5e76a.js"
+  }
+]);
