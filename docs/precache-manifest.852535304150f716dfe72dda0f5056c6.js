3893af6d85335123f79cf169b362984f40895bce
Karol Stopyra <stopyransky@gmail.com>
2020-03-18 18:41:57 +0100

    updates for build

---
+++ docs/precache-manifest.852535304150f716dfe72dda0f5056c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76850210891b083cfb8f5e9ab4c9ede",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "6c14a6f35ff57177629a",
+    "url": "/covid-19/static/css/main.3b045784.chunk.css"
+  },
+  {
+    "revision": "6811c11d99f187648f82",
+    "url": "/covid-19/static/js/2.ea9a3200.chunk.js"
+  },
+  {
+    "revision": "2d80e19561db3e1f4a41498474a39063",
+    "url": "/covid-19/static/js/2.ea9a3200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c14a6f35ff57177629a",
+    "url": "/covid-19/static/js/main.5cb646bf.chunk.js"
+  },
+  {
+    "revision": "54fcbfcaa259fe20686e",
+    "url": "/covid-19/static/js/runtime-main.aa282bec.js"
+  }
+]);
